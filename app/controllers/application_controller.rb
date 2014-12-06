a28bd1ff3995bc997423489ea77b97e0d93ac016
Sam Moore <samandmoore@gmail.com>
2014-12-06 16:07:14 -0500

    added simple user system with persistent cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  private
+
+  def current_user
+    @current_user ||= (User.find_by(id: cookies.permanent.signed[:user_id]) || User.create!)
+    cookies.permanent.signed[:user_id] ||= @current_user.id
+    @current_user
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141206195535) do
+ActiveRecord::Schema.define(version: 20141206210331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
 
   create_table "rooms", id: :uuid, default: "uuid_generate_v4()", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", id: :uuid, default: "uuid_generate_v4()", force: true do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
