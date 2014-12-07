7c57323ef6001736d66e62270e05282e47f0e4a3
Alan Norton <me@alannorton.com>
2014-12-07 16:36:05 -0500

    obfuscation with mashape

---
+++ lib/filters/obfuscate_unicode.rb
@@ -0,0 +1,10 @@
+class Filters::ObfuscateUnicode
+
+	def apply(input)
+    response = Faraday.get "https://bheithaus-unicode-obfuscator.p.mashape.com/obfuscate?level=3&word=#{input}" do |req|
+      req.headers["X-Mashape-Key"] = "rjmfOTqpp1mshxOBL4QtN6J1EOjpp11UQSnjsnN9f5K3slENR6"
+    end
+    JSON.parse(response.body)['obfuscation']
+	end
+
+end
