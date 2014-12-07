2e08efa73341552b9e9850ac419dde0a46f37ada
Sam Moore <samandmoore@gmail.com>
2014-12-07 15:49:03 -0500

    constantizd the timeout

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ActiveRecord::Base
   has_many :room_presences
-  has_many :active_room_presences, -> { where("room_presences.updated_at > ?", 2.minutes.ago) }, class_name: 'RoomPresence'
+  has_many :active_room_presences, -> { merge(RoomPresence.active) }, class_name: 'RoomPresence'
   has_many :members, through: :active_room_presences
 
   def build_message(opts = {})
--- app/models/room_presence.rb
+++ app/models/room_presence.rb
@@ -1,4 +1,7 @@
 class RoomPresence < ActiveRecord::Base
+  TIMEOUT = 10.seconds
   belongs_to :room
   belongs_to :member, class_name: 'User'
+
+  scope :active, -> { where("room_presences.updated_at > ?", TIMEOUT.ago) }
 end
